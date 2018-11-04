76181af88c198f7884ef73304f4a2191d519b3ad
KeyeZhang <keyez2@illinois.edu>
2018-11-03 19:56:58 -0500

    1st test succeed

--- src/receiver_main.cpp
+++ src/receiver_main.cpp
@@ -17,6 +17,7 @@
 #include <pthread.h>
 #include <string>
 #include <map>
+#include <iostream>
 
 using namespace std;
 
@@ -54,12 +55,14 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
     long long int orgPlanSeq = 0;
 
     char buf[1600];
-    FILE* fp;
+    memset(buf, 0, sizeof(buf));
+    FILE* fp = fopen(destinationFile, "wb");
 
     struct sockaddr_in their_addr; //sender's address, get filled from recvfrom function(how does this work?)
     int numbytes = 0;
     socklen_t their_addr_size = sizeof(their_addr);
     //ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,struct sockaddr *src_addr, socklen_t *addrlen);
+
     while (1) {
         if ((numbytes = recvfrom(s, buf, sizeof(buf), 0, (struct sockaddr *)&their_addr, &their_addr_size)) < 0 ) {
             diep("Finished: receive ends!");
@@ -68,18 +71,23 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         char seqBuf[32];
         long long int currSeqNum;
         char contentBuf[1600];
+	    memset(contentBuf, 0, sizeof(contentBuf));
         int seqI;
         int contentI;
 
         //1.get seq num
-        memcpy(&currSeqNum, buf, 8);
+        memcpy(&currSeqNum, buf, sizeof(long long int));
 
         //2.get content
-        memcpy(&contentBuf, buf + 12, sizeof(buf) - 12);
+        memcpy(&contentBuf, buf + 12, numbytes - 12);
         string contentStr = contentBuf; //c++
-
+        cout << buf + 12 << endl;
+        cout << "recv bytes: " << numbytes << endl;
+        cout << "seqnum: " << currSeqNum << endl;
+        cout << "content: " << contentStr << endl;
         //a packet that arrives too early, buffer it, and send the dup ack
         if (currSeqNum > orgPlanSeq && (receivedBufferedMap.find(currSeqNum) == receivedBufferedMap.end() || receivedBufferedMap[currSeqNum] == "")) {
+	    cout << "arrive early" << endl;
             receivedBufferedMap[currSeqNum] = contentStr;
             //under this condition: send back dup ack
             char ackChars[40];
@@ -92,12 +100,15 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         }
         //a packet which is expected, deliver it, and go forward to deliver any buffered packets right behind it as well
         else if (currSeqNum == orgPlanSeq) {
+	    cout << "arrive on time" << endl;
             // write to file and deliver
-            fwrite(contentBuf, 1, strlen(contentBuf), fp);
+            fwrite(contentBuf, sizeof(char), numbytes - 12, fp);
             fflush(fp);
             //release the buffered packets and write them all
             long long int nextPktIdx = currSeqNum + 1;
-            while (receivedBufferedMap.find(nextPktIdx) != receivedBufferedMap.end() && receivedBufferedMap[nextPktIdx].length() != 0) {
+            cout << "end writing 1" << endl;
+            while (receivedBufferedMap.find(nextPktIdx) != receivedBufferedMap.end() &&      receivedBufferedMap[nextPktIdx].length() != 0) {
+		        cout << "start writing 2" << endl;
                 fwrite(receivedBufferedMap[nextPktIdx].c_str(), 1, receivedBufferedMap[nextPktIdx].length(), fp);
                 fflush(fp);
 
@@ -105,6 +116,7 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
                 receivedBufferedMap[nextPktIdx] = "";
                 nextPktIdx ++;
             }
+		    cout << "out" << endl;
             lastAckedNum = nextPktIdx - 1;
             orgPlanSeq = nextPktIdx;
         }
@@ -113,15 +125,14 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
         memcpy(ackChars, &lastAckedNum, 8);
         // sprintf(ackChars, "%lld", lastAckedNum);
         // string ackCharsTmp = ackChars;
-        sendto(s, (void*)ackChars, sizeof(long long int), 0, (struct sockaddr *)&their_addr, their_addr_size);
+        sendto(s, ackChars, sizeof(long long int), 0, (struct sockaddr *)&their_addr, their_addr_size);
         // if (numbytes < 0) {
         //     diep("send ack error");
         // }
     }
 
-
     close(s);
-	printf("%s received.", destinationFile);
+    printf("%s received.", destinationFile);
     return;
 }
 
